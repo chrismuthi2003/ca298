c77f40512bdb608ea0d8bde2fd2f3d951a63cce1
Christopher Muthi <christopher.muthi2@mail.dcu.ie>
2023-08-25 17:57:27 +0100

    transferred files from gitlab

---
+++ week5/javascript.js
@@ -0,0 +1,45 @@
+true false
+1, 2, 3
+"Hello", "Hello"
+1.40
+[1, 2, 3]
+[1, "Hello", 7.8]
+new Data()
+{
+    "key":1
+    "name": "Michael"
+}
+
+var x = 5;
+y=6
+
+let x = 5; // create a mutable variable
+x = x + 1 //
+
+const x = 6 // non mutable
+
+
+
+1 === "1" = false
+!=
+>=
+<=
+>
+<
+
+a and b
+a or b
+
+a && b // and statment
+b || b // or statment:wq
+
+
+if(1==1)
+{
+    console.log("1=1"); // print statement
+    x+=1
+}
+
+if(1==1)
+    console.log("1=1")
+    x+=1
